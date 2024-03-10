cbd9e386085bd4285dd1c14ffd6b79d449557c78
Ameer Arsala <ameer.arsala03@gmail.com>
2024-03-10 12:05:47 -0700

    AUTO PYTHON ENV DETECTIONgit status! Includes micromamba (GOAT)

--- init.lua
+++ init.lua
@@ -24,3 +24,141 @@ require("config.lazy")
 -- ignore_install = {}, -- List of languages to ignore during installation
 -- auto_install = true, -- Automatically install parsers
 -- })
+
+require("nvim-treesitter.configs").setup({
+  autotag = {
+    enable = true,
+  },
+  highlight = {
+    enable = true, -- Enable syntax highlighting
+  },
+  indent = {
+    enable = true,
+    disable = { "python", "css", "rust" },
+  },
+  --ensure_installed = "all",
+  ensure_installed = {
+    "java",
+    "cpp",
+    "rust",
+    "python",
+    "lua",
+    "html",
+    "json",
+    "dockerfile",
+    "yaml",
+    "css",
+    "javascript",
+    "typescript",
+    "svelte",
+    "julia",
+    "c",
+    "vue",
+    "glsl",
+    "hlsl",
+    "c_sharp",
+    "astro",
+    "vim",
+    "zig",
+    "toml",
+    "sql",
+    "ruby",
+    --"pip-requirements",
+    "php",
+    "r",
+    "prisma",
+    "graphql",
+    "go",
+    "nim",
+    "latex",
+    "haskell",
+    "gitignore",
+    "gdscript",
+    "gdshader",
+    "elixir",
+    "d",
+    "dart",
+    "csv",
+    "cuda",
+    "asm",
+    "bash",
+    "bibtex",
+    "arduino",
+  },
+})
+
+-- Function to execute a shell command and return its output
+function execute_command(command)
+  local handle = io.popen(command)
+  local result = handle:read("*a")
+  handle:close()
+  return result
+end
+
+-- Function to parse the output of `micromamba env list` and find the active environment
+function get_active_micromamba_env()
+  local output = execute_command("$MAMBA_EXE env list")
+  --vim.notify("envs: " .. output)
+  for line in output:gmatch("[^\r\n]+") do
+    if line:find("*") then
+      local env_name = line:match("^%s*(%S+)")
+      return env_name
+    end
+  end
+  return nil
+end
+
+-- Function to get the path to the active micromamba environment
+local function get_python_path()
+  local active_micromamba_env_name = get_active_micromamba_env()
+  if active_micromamba_env_name == nil then
+    return "/usr/bin/python3"
+  end
+  -- Otherwise, it is a micromamba environment
+  local mamba_root_prefix = os.getenv("MAMBA_ROOT_PREFIX")
+  local active_env_path = mamba_root_prefix .. "/envs/" .. active_micromamba_env_name .. "/bin/python"
+  return active_env_path
+end
+
+-- Get the path to the active python environment
+local python_path = get_python_path()
+vim.notify("Python Path: " .. python_path)
+
+local lspconfig = require("lspconfig")
+
+-- Disable hover in ruff-lsp in favor of pyright
+local on_attach = function(client, bufnr)
+  if client.name == "ruff_lsp" then
+    client.server_capabilities.hoverProvider = false
+  end
+end
+
+-- Configure ruff-lsp
+lspconfig.ruff_lsp.setup({
+  on_attach = on_attach,
+  init_options = {
+    settings = {
+      args = {}, -- Any extra CLI arguments for `ruff` go here.
+      logLevel = "info",
+      interpreter = python_path,
+      --interpreter=[python_path]
+    },
+  },
+})
+
+-- Configure pyright to ignore all files for analysis to exclusively use Ruff for linting
+lspconfig.pyright.setup({
+  settings = {
+    pyright = {
+      disableOrganizeImports = true, -- Using Ruff's import organizer
+    },
+    python = {
+      analysis = {
+        ignore = { "*" }, -- Ignore all files for analysis (pyright, you suck!!!)
+        typeCheckingMode = "standard",
+        useLibraryCodeForTypes = true,
+      },
+      pythonPath = python_path,
+    },
+  },
+})
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
-local config = {
+local cfg = {
   "nvim-treesitter/nvim-treesitter",
   dependencies = {
-    "hiphish/rainbow-delimiters.nvim",
+    --"hiphish/rainbow-delimiters.nvim",
     "nvim-treesitter/nvim-treesitter-textobjects",
     "windwp/nvim-ts-autotag",
     "nvim-treesitter/nvim-treesitter-refactor",
@@ -10,8 +10,8 @@ local config = {
   },
   build = ":TSUpdate",
   config = function()
-    local colors = require("config/themes").colors
-    local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+    --local colors = require("config/themes").colors
+    --local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
 
     -- vim way: ; goes to the direction you were moving.
     --vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
@@ -39,7 +39,40 @@ local config = {
         "css",
         "javascript",
         "typescript",
+        "svelte",
+        "julia",
+        "c",
         "vue",
+        "glsl",
+        "hlsl",
+        "c_sharp",
+        "astro",
+        "vim",
+        "zig",
+        "toml",
+        "sql",
+        "ruby",
+        --"pip-requirements",
+        "php",
+        "r",
+        "prisma",
+        "graphql",
+        "go",
+        "nim",
+        "latex",
+        "haskell",
+        "gitignore",
+        "gdscript",
+        "gdshader",
+        "elixir",
+        "d",
+        "dart",
+        "csv",
+        "cuda",
+        "asm",
+        "bash",
+        "bibtex",
+        "arduino",
       },
       refactor = {
         highlight_definitions = {
@@ -62,97 +95,96 @@ local config = {
       },
       -- rainbow = {
       --   colors = {
+      --     colors.grey5,
       --     colors.magenta,
-      --     colors.cyan,
       --     colors.yellow,
       --     colors.orange,
       --     colors.pink,
-      --     colors.blue,
-      --     colors.green,
       --   },
       --   enable = true,
       --   extended_mode = true,
       --   max_file_lines = nil,
       -- },
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ["aa"] = { query = "@parameter.outer", desc = "Select outer part of a argument/parameter" },
-            ["ia"] = { query = "@parameter.inner", desc = "Select inner part of a argument/parameter" },
-            ["ab"] = { query = "@block.outer", desc = "Select outer part of a block" },
-            ["ib"] = { query = "@block.inner", desc = "Select inner part of a block" },
-            ["am"] = { query = "@function.outer", desc = "Select outer part of a method" },
-            ["im"] = { query = "@function.inner", desc = "Select inner part of a method" },
-            ["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
-            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
-            ["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
-            ["ii"] = { query = "@conditional.inner", desc = "Select inner part of a conditional" },
-            ["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
-            ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
-          },
-        },
-        move = {
-          enable = true,
-          set_jumps = true, -- whether to set jumps in the jumplist
-          goto_next_start = {
-            ["]m"] = { query = "@function.outer", desc = "Next method start" },
-            ["]c"] = { query = "@class.outer", desc = "Next class start" },
-            ["]a"] = { query = "@parameter.outer", desc = "Next argument/parameter start" },
-            ["]b"] = { query = "@block.outer", desc = "Next block start" },
-            ["]i"] = { query = "@conditional.outer", desc = "Next conditional start" },
-            ["]l"] = { query = "@loop.outer", desc = "Next loop start" },
-          },
-          goto_next_end = {
-            ["]M"] = { query = "@function.outer", desc = "Next method end" },
-            ["]C"] = { query = "@class.outer", desc = "Next class end" },
-            ["]A"] = { query = "@parameter.inner", desc = "Next argument/parameter end" },
-            ["]B"] = { query = "@block.outer", desc = "Next block end" },
-            ["]I"] = { query = "@conditional.outer", desc = "Next conditional end" },
-            ["]L"] = { query = "@loop.outer", desc = "Next loop end" },
-          },
-          goto_previous_start = {
-            ["[m"] = { query = "@function.outer", desc = "Previous method start" },
-            ["[c"] = { query = "@class.outer", desc = "Previous class start" },
-            ["[a"] = { query = "@parameter.outer", desc = "Previous argument/parameter start" },
-            ["[b"] = { query = "@block.outer", desc = "Previous block start" },
-            ["[i"] = { query = "@conditional.outer", desc = "Previous conditional start" },
-            ["[l"] = { query = "@loop.outer", desc = "Previous loop start" },
-          },
-          goto_previous_end = {
-            ["[M"] = { query = "@function.outer", desc = "Previous method end" },
-            ["[C"] = { query = "@class.outer", desc = "Previous class end" },
-            ["[A"] = { query = "@parameter.inner", desc = "Previous argument/parameter end" },
-            ["[B"] = { query = "@block.outer", desc = "Previous block end" },
-            ["[I"] = { query = "@conditional.outer", desc = "Previous conditional end" },
-            ["[L"] = { query = "@loop.outer", desc = "Previous loop end" },
-          },
-        },
-      },
-      playground = {
-        enable = true,
-        disable = {},
-        updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-        persist_queries = false, -- Whether the query persists across vim sessions
-        keybindings = {
-          toggle_query_editor = "o",
-          toggle_hl_groups = "i",
-          toggle_injected_languages = "t",
-          toggle_anonymous_nodes = "a",
-          toggle_language_display = "I",
-          focus_language = "f",
-          unfocus_language = "F",
-          update = "R",
-          goto_node = "<cr>",
-          show_help = "?",
-        },
-      },
-      matchup = {
-        -- enables vim-matchup integration
-        enable = true,
-        enable_quotes = true,
-      },
+      -- textobjects = {
+      --   select = {
+      --     enable = true,
+      --     lookahead = true,
+      --     keymaps = {
+      --       ["aa"] = { query = "@parameter.outer", desc = "Select outer part of a argument/parameter" },
+      --       ["ia"] = { query = "@parameter.inner", desc = "Select inner part of a argument/parameter" },
+      --       ["ab"] = { query = "@block.outer", desc = "Select outer part of a block" },
+      --       ["ib"] = { query = "@block.inner", desc = "Select inner part of a block" },
+      --       ["am"] = { query = "@function.outer", desc = "Select outer part of a method" },
+      --       ["im"] = { query = "@function.inner", desc = "Select inner part of a method" },
+      --       ["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
+      --       ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
+      --       ["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
+      --       ["ii"] = { query = "@conditional.inner", desc = "Select inner part of a conditional" },
+      --       ["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
+      --       ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
+      --     },
+      --   },
+      --   move = {
+      --     enable = true,
+      --     set_jumps = true, -- whether to set jumps in the jumplist
+      --     goto_next_start = {
+      --       ["]m"] = { query = "@function.outer", desc = "Next method start" },
+      --       ["]c"] = { query = "@class.outer", desc = "Next class start" },
+      --       ["]a"] = { query = "@parameter.outer", desc = "Next argument/parameter start" },
+      --       ["]b"] = { query = "@block.outer", desc = "Next block start" },
+      --       ["]i"] = { query = "@conditional.outer", desc = "Next conditional start" },
+      --       ["]l"] = { query = "@loop.outer", desc = "Next loop start" },
+      --     },
+      --     goto_next_end = {
+      --       ["]M"] = { query = "@function.outer", desc = "Next method end" },
+      --       ["]C"] = { query = "@class.outer", desc = "Next class end" },
+      --       ["]A"] = { query = "@parameter.inner", desc = "Next argument/parameter end" },
+      --       ["]B"] = { query = "@block.outer", desc = "Next block end" },
+      --       ["]I"] = { query = "@conditional.outer", desc = "Next conditional end" },
+      --       ["]L"] = { query = "@loop.outer", desc = "Next loop end" },
+      --     },
+      --     goto_previous_start = {
+      --       ["[m"] = { query = "@function.outer", desc = "Previous method start" },
+      --       ["[c"] = { query = "@class.outer", desc = "Previous class start" },
+      --       ["[a"] = { query = "@parameter.outer", desc = "Previous argument/parameter start" },
+      --       ["[b"] = { query = "@block.outer", desc = "Previous block start" },
+      --       ["[i"] = { query = "@conditional.outer", desc = "Previous conditional start" },
+      --       ["[l"] = { query = "@loop.outer", desc = "Previous loop start" },
+      --     },
+      --     goto_previous_end = {
+      --       ["[M"] = { query = "@function.outer", desc = "Previous method end" },
+      --       ["[C"] = { query = "@class.outer", desc = "Previous class end" },
+      --       ["[A"] = { query = "@parameter.inner", desc = "Previous argument/parameter end" },
+      --       ["[B"] = { query = "@block.outer", desc = "Previous block end" },
+      --       ["[I"] = { query = "@conditional.outer", desc = "Previous conditional end" },
+      --       ["[L"] = { query = "@loop.outer", desc = "Previous loop end" },
+      --     },
+      --   },
+      -- },
+      -- playground = {
+      --   enable = true,
+      --   disable = {},
+      --   updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+      --   persist_queries = false, -- Whether the query persists across vim sessions
+      --   keybindings = {
+      --     toggle_query_editor = "o",
+      --     toggle_hl_groups = "i",
+      --     toggle_injected_languages = "t",
+      --     toggle_anonymous_nodes = "a",
+      --     toggle_language_display = "I",
+      --     focus_language = "f",
+      --     unfocus_language = "F",
+      --     update = "R",
+      --     goto_node = "<cr>",
+      --     show_help = "?",
+      --   },
+      -- },
+      -- matchup = {
+      --   -- enables vim-matchup integration
+      --   enable = true,
+      --   enable_quotes = true,
+      -- },
+
       -- incremental_selection = {
       --     enable = true,
       --     keymaps = {
