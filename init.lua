1d62ec39e1a76231e253a48dd9c185bd2f843c2c
Ameer Arsala <ameer.arsala03@gmail.com>
2024-08-03 22:00:48 -0700

    ignoring the search of these random ass file formats

--- init.lua
+++ init.lua
@@ -26,6 +26,13 @@ require("telescope").setup({
       "node_modules/.*", -- Ignore everything under node_modules
       ".git/.*", -- Ignore everything under .git
       ".pixi/.*", -- Ignore everything under .pixi
+      "__pycache__/.*", -- Ignore everything under __pycache__
+      ".ipynb_checkpoints/.*", -- Ignore everything under .ipynb_checkpoints
+      ".mypy_cache/.*", -- Ignore everything under .mypy_cache
+      "ruff_cache/.*", -- Ignore everything under ruff_cache
+      ".pytest_cache/.*", -- Ignore everything under .pytest_cache
+      ".venv/.*", -- Ignore everything under .venv
+      "node_modules/.*", -- Ignore everything under node_modules
     },
   },
 })
