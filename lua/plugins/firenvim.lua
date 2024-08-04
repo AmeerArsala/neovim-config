e17aedf0fb22928f1382f25a65a1694efa151035
Ameer Arsala <ameer.arsala03@gmail.com>
2024-08-04 15:45:37 -0700

    disabled firenvim

--- lua/plugins/firenvim.lua
+++ lua/plugins/firenvim.lua
@@ -1,25 +1,30 @@
+-- firenvim can be configured later
+if true then
+  return {}
+end
+
 return {
-    "glacambre/firenvim",
-  
-    -- Lazy load firenvim
-    -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
-    lazy = not vim.g.started_by_firenvim,
-    module = false,
-    build = function()
-      vim.fn["firenvim#install"](0)
-    end,
-    init = function()
-      vim.g.firenvim_config = {
-        globalSettings = { alt = "all" },
-        localSettings = {
-          [".*"] = {
-            cmdline = "firenvim",
-            content = "text",
-            priority = 0,
-            selector = 'textarea:not([readonly]):not([class="handsontableInput"]), div[role="textbox"]',
-            takeover = "never",
-          },
+  "glacambre/firenvim",
+
+  -- Lazy load firenvim
+  -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
+  lazy = not vim.g.started_by_firenvim,
+  module = false,
+  build = function()
+    vim.fn["firenvim#install"](0)
+  end,
+  init = function()
+    vim.g.firenvim_config = {
+      globalSettings = { alt = "all" },
+      localSettings = {
+        [".*"] = {
+          cmdline = "firenvim",
+          content = "text",
+          priority = 0,
+          selector = 'textarea:not([readonly]):not([class="handsontableInput"]), div[role="textbox"]',
+          takeover = "never",
         },
-      }
-    end,
-  }
+      },
+    }
+  end,
+}
